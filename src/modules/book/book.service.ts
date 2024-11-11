73fd6ddc954dfa9c9b0e4af7becbae34f210fc8f
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2024-11-11 12:20:18 +0545

    made the search insensitive using ILike

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -5,7 +5,7 @@ import { bookEntity } from 'src/model/book.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { categoryEntity } from 'src/model/category.entity';
 import { userEntity } from 'src/model/user.entity';
-import { In, Like, Repository } from 'typeorm';
+import { ILike, In, Like, Repository } from 'typeorm';
 import { PaginationDto } from 'src/helper/utils/pagination.dto';
 import { viewEntity } from 'src/model/view.entity';
 
@@ -104,8 +104,8 @@ export class BookService {
   async searchBooks(query: string): Promise<bookEntity[]> {
     return await this.bookRepo.find({
       where: [
-        { title: Like(`%${query}%`) },
-        { author: Like(`%${query}%`) },
+        { title: ILike(`%${query}%`) },
+        { author: ILike(`%${query}%`) },
       ],
     });
   }
